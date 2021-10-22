855f4b5447b8aa2675ce9f2a39c1646bdc001eb8
soloma70 <soloma70@gmail.com>
2021-10-22 21:27:52 +0300

    Format locators

--- pages/locators.py
+++ pages/locators.py
@@ -1,19 +1,20 @@
 from selenium.webdriver.common.by import By
 
+
 class AuthLocators:
     AUTH_EMAIL = (By.ID, 'email')
     AUTH_PASS = (By.ID, 'pass')
     AUTH_BTN = (By.CLASS_NAME, 'btn-success')
 
+
 class StartLocators:
     SUBMIT_BTN = (By.CLASS_NAME, 'btn.btn-success')
+
 
 class NewUserLocators:
     NEW_USER_BTN = (By.CSS_SELECTOR, 'a[href="/login"]')
 
+
 class AllPetsLocators:
     MY_PETS_BTN = (By.XPATH, "//a[@href='/my_pets']")
     EXIT_BTN = (By.XPATH, "//button[@class='btn btn-outline-secondary']")
-
-class MyPetsLocators:
-    pass
