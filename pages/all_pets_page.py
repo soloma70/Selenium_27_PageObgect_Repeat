d779708a84c582ce2dd5d336f8b61043e5214ef7
soloma70 <soloma70@gmail.com>
2021-10-22 21:30:08 +0300

    Отформатирован файл All Pets Page

--- pages/all_pets_page.py
+++ pages/all_pets_page.py
@@ -3,6 +3,7 @@ from .locators import AllPetsLocators
 from .url_list import PetFriend
 import os
 
+
 class AllPetsPage(BasePage):
     def __init__(self, driver, timeout=10):
         super().__init__(driver, timeout)
@@ -11,8 +12,8 @@ class AllPetsPage(BasePage):
         self.my_pets_btn = driver.find_element(*AllPetsLocators.MY_PETS_BTN)
         self.exit_btn = driver.find_element(*AllPetsLocators.EXIT_BTN)
 
-    def my_pets_btn_click(self):
+    def my_pets_click(self):
         self.my_pets_btn.click()
 
-    def exit_btn_click(self):
+    def exit_click(self):
         self.exit_btn.click()
