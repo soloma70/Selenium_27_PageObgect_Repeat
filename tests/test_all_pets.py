c1a6497ab09a004e380c3a84c0a1c8c15c899cca
soloma70 <soloma70@gmail.com>
2021-10-22 21:29:32 +0300

    Добавлены тесты в All Pets

--- tests/test_all_pets.py
+++ tests/test_all_pets.py
@@ -2,21 +2,30 @@ import time
 from selenium.webdriver.common.action_chains import ActionChains
 from pages.url_list import PetFriend
 from pages.all_pets_page import AllPetsPage
+from settings import valid_name
+
 
 def test_visit_all_pets(web_driver_with_cookies):
     web_driver_with_cookies.get(PetFriend.ALL_PETS_URL)
     assert web_driver_with_cookies.title == 'PetFriends: My Pets'
     # time.sleep(3)
 
+
 def test_scroll(web_driver_with_cookies):
     # web_driver_with_cookies.get(PetFriend.ALL_PETS_URL)
     # assert web_driver_with_cookies.title == 'PetFriends: My Pets'
     web_driver_with_cookies.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(5)
+    time.sleep(2)
+    web_driver_with_cookies.execute_script("window.scrollTo(document.body.scrollHeight, 0);")
+    time.sleep(2)
+
 
 def test_visit_my_pets(web_driver_with_cookies):
     page = AllPetsPage(web_driver_with_cookies)
-    page.my_pets_btn_click()
-    assert page.get_relative_link() == '/my_pets', 'Login error'
+    page.my_pets_click()
     web_driver_with_cookies.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(10)
+    time.sleep(2)
+    web_driver_with_cookies.execute_script("window.scrollTo(document.body.scrollHeight, 0);")
+    time.sleep(2)
+    assert page.get_relative_link() == '/my_pets', 'Login error'
+    assert web_driver_with_cookies.find_element_by_tag_name('h2').text == valid_name, 'Non valid name'
