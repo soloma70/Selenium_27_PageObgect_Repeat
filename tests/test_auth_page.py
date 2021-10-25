eb634e532ec402f668d4110bbcbd431a95632832
soloma70 <soloma70@gmail.com>
2021-10-25 23:11:40 +0300

    Реформат

--- tests/test_auth_page.py
+++ tests/test_auth_page.py
@@ -2,16 +2,17 @@ import pytest
 from pages.auth_page import AuthPage
 from settings import valid_email, valid_password
 
+
 @pytest.mark.parametrize("nv_email",
-                        ['12356@g.com', AuthPage.generate_string(255), AuthPage.generate_string(1001)
-                        , AuthPage.russian_chars(), AuthPage.russian_chars().upper(), AuthPage.chinese_chars()
-                        , AuthPage.special_chars(), 123]
-                        , ids= ['any', '255 sym', '> 1000 sym', 'russian', 'RUSSIAN', 'chinese', 'specials', 'digit'])
+                         ['12356@g.com', AuthPage.generate_string(255), AuthPage.generate_string(1001)
+                             , AuthPage.russian_chars(), AuthPage.russian_chars().upper(), AuthPage.chinese_chars()
+                             , AuthPage.special_chars(), 123]
+    , ids=['any', '255 sym', '> 1000 sym', 'russian', 'RUSSIAN', 'chinese', 'specials', 'digit'])
 @pytest.mark.parametrize("nv_password",
-                        ['123456', AuthPage.generate_string(255), AuthPage.generate_string(1001)
-                        , AuthPage.russian_chars(), AuthPage.russian_chars().upper(), AuthPage.chinese_chars()
-                        , AuthPage.special_chars(), 123]
-                        , ids= ['any', '255 sym', '> 1000 sym', 'russian', 'RUSSIAN', 'chinese', 'specials', 'digit'])
+                         ['123456', AuthPage.generate_string(255), AuthPage.generate_string(1001)
+                             , AuthPage.russian_chars(), AuthPage.russian_chars().upper(), AuthPage.chinese_chars()
+                             , AuthPage.special_chars(), 123]
+    , ids=['any', '255 sym', '> 1000 sym', 'russian', 'RUSSIAN', 'chinese', 'specials', 'digit'])
 def test_auth_page_negativ(web_driver, nv_email, nv_password):
     page = AuthPage(web_driver)
     page.enter_email(nv_email)
@@ -19,7 +20,6 @@ def test_auth_page_negativ(web_driver, nv_email, nv_password):
     page.btn_click()
     print('\nЗапрет на авторизацию. Проверено')
     assert page.get_relative_link() != '/all_pets', 'Login error'
-
 
 
 def test_auth_page_with_valid_data(web_driver):
