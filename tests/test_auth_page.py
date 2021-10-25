5827f8bb0e58352f003be52cbeba31e0dd8a5d4d
soloma70 <soloma70@gmail.com>
2021-10-25 23:07:55 +0300

    Добавлена параметризация негативного теста

--- tests/test_auth_page.py
+++ tests/test_auth_page.py
@@ -1,14 +1,25 @@
+import pytest
 from pages.auth_page import AuthPage
-from settings import valid_email, valid_password, non_valid_email, non_valid_password
+from settings import valid_email, valid_password
 
-
-def test_auth_page_negativ(web_driver):
+@pytest.mark.parametrize("nv_email",
+                        ['12356@g.com', AuthPage.generate_string(255), AuthPage.generate_string(1001)
+                        , AuthPage.russian_chars(), AuthPage.russian_chars().upper(), AuthPage.chinese_chars()
+                        , AuthPage.special_chars(), 123]
+                        , ids= ['any', '255 sym', '> 1000 sym', 'russian', 'RUSSIAN', 'chinese', 'specials', 'digit'])
+@pytest.mark.parametrize("nv_password",
+                        ['123456', AuthPage.generate_string(255), AuthPage.generate_string(1001)
+                        , AuthPage.russian_chars(), AuthPage.russian_chars().upper(), AuthPage.chinese_chars()
+                        , AuthPage.special_chars(), 123]
+                        , ids= ['any', '255 sym', '> 1000 sym', 'russian', 'RUSSIAN', 'chinese', 'specials', 'digit'])
+def test_auth_page_negativ(web_driver, nv_email, nv_password):
     page = AuthPage(web_driver)
-    page.enter_email(non_valid_email)
-    page.enter_pass(non_valid_password)
+    page.enter_email(nv_email)
+    page.enter_pass(nv_password)
     page.btn_click()
-    print('\nПроверяем, что мы не авторизовались')
+    print('\nЗапрет на авторизацию. Проверено')
     assert page.get_relative_link() != '/all_pets', 'Login error'
+
 
 
 def test_auth_page_with_valid_data(web_driver):
@@ -16,5 +27,5 @@ def test_auth_page_with_valid_data(web_driver):
     page.enter_email(valid_email)
     page.enter_pass(valid_password)
     page.btn_click()
-    print('\nПроверяем, что мы авторизовались и находимся на странице всех питомцев')
+    print('\nУспешная авторизация и переход на страницу всех питомцев')
     assert page.get_relative_link() == '/all_pets', 'Login error'
