f523412d611b6585f88595226351b82d20aae940
soloma70 <soloma70@gmail.com>
2021-10-25 23:07:47 +0300

    Добавлена параметризация негативного теста

--- pages/auth_page.py
+++ pages/auth_page.py
@@ -20,3 +20,16 @@ class AuthPage(BasePage):
 
     def btn_click(self):
         self.btn.click()
+
+    # Вспомашательные функции для добавления различных строк
+    def generate_string(n):
+        return "x" * n
+
+    def russian_chars():
+        return 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+    def chinese_chars():
+        return '的一是不了人我在有他这为之大来以个中上们'
+
+    def special_chars():
+        return '|\\/!@#$%^&*()-_=+`~?"№;:[]{}'
