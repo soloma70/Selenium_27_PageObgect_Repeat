b9b6fe8bc81ef67d33767ee4cada63353870733e
soloma70 <soloma70@gmail.com>
2021-10-25 21:49:58 +0300

    Перенесена фикстура без авторизации

--- tests/test_auth_page.py
+++ tests/test_auth_page.py
@@ -1,18 +1,6 @@
-import pytest
-from selenium import webdriver
 from pages.auth_page import AuthPage
 from settings import valid_email, valid_password
 
-
-@pytest.fixture(scope='module')
-def web_driver(request):
-     '''Фикстура загружает веб-драйвер Хром, меняет размер окна, устанавливает неявное ожидание,
-     после выполнения основного кода закрывает браузер'''
-     web_driver = webdriver.Chrome("C:\\1\\chromedriver.exe")
-     web_driver.set_window_size(1280, 960)
-     web_driver.implicitly_wait(3)
-     yield web_driver
-     web_driver.quit()
 
 def test_auth_page_with_valid_data(web_driver):
     page = AuthPage(web_driver)
