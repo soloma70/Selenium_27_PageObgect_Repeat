73377e2a13c924568603e6fb894d6717899eb65c
soloma70 <soloma70@gmail.com>
2021-10-25 22:14:25 +0300

    Общий реформат

--- tests/test_auth_page.py
+++ tests/test_auth_page.py
@@ -1,5 +1,14 @@
 from pages.auth_page import AuthPage
-from settings import valid_email, valid_password
+from settings import valid_email, valid_password, non_valid_email, non_valid_password
+
+
+def test_auth_page_negativ(web_driver):
+    page = AuthPage(web_driver)
+    page.enter_email(non_valid_email)
+    page.enter_pass(non_valid_password)
+    page.btn_click()
+    print('\nПроверяем, что мы не авторизовались')
+    assert page.get_relative_link() != '/all_pets', 'Login error'
 
 
 def test_auth_page_with_valid_data(web_driver):
