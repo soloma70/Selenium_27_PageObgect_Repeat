ba1b4eed6fae51d3cd3bab5588839dfeebd3bce8
soloma70 <soloma70@gmail.com>
2021-10-25 21:08:19 +0300

    Добавлены локаторы в AllPetsLocators

--- pages/locators.py
+++ pages/locators.py
@@ -17,8 +17,12 @@ class NewUserLocators:
 
 class AllPetsLocators:
     MY_PETS_BTN = (By.XPATH, "//a[@href='/my_pets']")
+    ALL_PETS_IMAGES = (By.CLASS_NAME, 'card-img-top')
+    ALL_PETS_NAMES = (By.CLASS_NAME, 'card-title')
+    ALL_PETS_DESC = (By.CLASS_NAME, 'card-text')
     EXIT_BTN = (By.XPATH, "//button[@class='btn btn-outline-secondary']")
 
 class MyPetsLocators:
     MY_PETS_BTN = (By.XPATH, "//a[@href='/my_pets']")
+    MY_PETS_NAME = (By.TAG_NAME, "h2")
     EXIT_BTN = (By.XPATH, "//button[@class='btn btn-outline-secondary']")
