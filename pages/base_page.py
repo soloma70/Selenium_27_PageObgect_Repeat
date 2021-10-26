6c76ae22df1461c862569d62c520d72a6a5c4bc9
soloma70 <soloma70@gmail.com>
2021-10-26 22:51:20 +0300

    Изменен таймаут по умолчанию

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,7 +2,7 @@ from urllib.parse import urlparse
 
 class BasePage(object):
     # Конструктор класса - метод init, получающий объект веб-драйвера, адрес страницы и время ожидания элемента
-    def __init__(self, driver, url, timeout=10):
+    def __init__(self, driver, url, timeout=5):
         self.driver = driver
         self.url = url
         self.driver.implicitly_wait(timeout)
