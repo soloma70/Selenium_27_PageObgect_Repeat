54d38d3ec1d35f191c2b968ef662d5f3ca2d3d4a
soloma70 <soloma70@gmail.com>
2021-10-27 18:12:43 +0300

    Добавлены данные добавления питомца

--- settings.py
+++ settings.py
@@ -1,3 +1,8 @@
 valid_name = "test227"
 valid_email = valid_name + '@gmail.com'
 valid_password = valid_name
+
+img_pet = '\\images\\cat11.jpg'
+name_pet = 'Матис Автотест'
+type_pet = 'Британец'
+age_pet = '9'
