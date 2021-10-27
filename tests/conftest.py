35e876e80249835bb3c51fe46f4216a717faa414
soloma70 <soloma70@gmail.com>
2021-10-28 00:38:45 +0300

    Добавлены Chrome Option User-Agent

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,19 @@
 import pytest, requests
 from selenium import webdriver
 from pages.url_list import PetFriend
-from settings import valid_email, valid_password
+from settings import valid_email, valid_password, web_driver_path
 
 @pytest.fixture(scope='module')
 def web_driver(request):
     '''Фикстура загружает веб-драйвер Хром, меняет размер окна, устанавливает неявное ожидание,
     после выполнения основного кода закрывает браузер'''
-    web_driver = webdriver.Chrome("C:\\1\\chromedriver.exe")
+
+    option = webdriver.ChromeOptions()
+    option.add_argument("User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36")
+    web_driver = webdriver.Chrome(executable_path=web_driver_path, options=option)
+    # Отключение обнаружения webdriver
+    # option.add_argument("--disable-blink-features=AutomationControlled")
+    # option.add_argument("--disable-web-security")
     web_driver.set_window_size(1280, 960)
     web_driver.implicitly_wait(3)
     yield web_driver
@@ -17,13 +23,19 @@ def web_driver(request):
 @pytest.fixture(scope='module')
 def web_driver_with_cookies(request):
     '''Фикстура загружает авторизуется, получает cookies и передает их в драйвер'''
+
     print('\nПолучаем ключ авторизации и куки...')
     response = requests.post(url=PetFriend.LOGIN_URL
                              , data={"email": valid_email, "pass": valid_password})
     assert response.status_code == 200, 'Запрос выполнен неуспешно'
     assert 'Cookie' in response.request.headers, 'В запросе не передан ключ авторизации'
 
-    web_driver = webdriver.Chrome("C:\\1\\chromedriver.exe")
+    option = webdriver.ChromeOptions()
+    option.add_argument(
+        "User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36")
+    web_driver = webdriver.Chrome(executable_path=web_driver_path, options=option)
+    # Отключение обнаружения webdriver
+    # option.add_argument("--disable-blink-features=AutomationControlled")
     web_driver.set_window_size(1280, 960)
     web_driver.implicitly_wait(3)
     web_driver.get(PetFriend.START_URL)
