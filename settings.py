56c61bad39aa26edc16c12f4d2edb951392763b8
soloma70 <soloma70@gmail.com>
2021-10-28 16:33:38 +0300

    Добавлен список питомцев

--- settings.py
+++ settings.py
@@ -4,7 +4,15 @@ valid_name = "test228"
 valid_email = valid_name + '@gmail.com'
 valid_password = valid_name
 
-img_pet = '\\images\\cat10.jpg'
-name_pet = 'Матюся А'
-type_pet = 'Британец'
-age_pet = '5'
+list_pest = [{'img': '\\images\\cat1.jpg', 'name': 'Matis', 'type': 'British', 'age': '3'},
+             {'img': '\\images\\cat2.jpg', 'name': 'Матис', 'type': 'Британец', 'age': '4'},
+             {'img': '\\images\\cat3.jpg', 'name': '地鼠', 'type': '英国短毛猫', 'age': '2'},
+             {'img': '\\images\\cat4.jpg', 'name': 'Лапочка', 'type': 'Беспородная', 'age': '0'},
+             {'img': '\\images\\cat5.jpg', 'name': 'Совенок', 'type': 'Сибирская', 'age': '0'},
+             {'img': '\\images\\cat6.jpg', 'name': 'Котожидкость', 'type': 'Двортерьер', 'age': '2'},
+             {'img': '\\images\\cat10.jpg', 'name': 'Love Is', 'type': 'Forever', 'age': '50'},
+             {'img': '\\images\\cat11.jpg', 'name': 'Матюся', 'type': 'Скотиш Страйт', 'age': '9'}]
+# img_pet = '\\images\\cat2.jpg'
+# name_pet = 'Матюся A'
+# type_pet = 'Короткошерстный'
+# age_pet = '4'
